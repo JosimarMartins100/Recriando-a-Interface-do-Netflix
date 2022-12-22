1afb9408486b6dec5ae5f6a1f78228ceda182fe2
Josimar Martins dev <josimarmartins2014@hotmail.com>
2022-12-22 01:07:09 -0300

    finalizando

---
+++ js/link1.js
@@ -0,0 +1,13 @@
+const url = 'https://www.youtube.com/watch?v=x5pZI-DmtrE'
+
+const btn = document.querySelector('#btn1')
+
+function openInNewtab(url) {
+    const win = window.open(url,'_blank')
+    win.focus()
+}
+
+btn.addEventListener('click', () => {
+    openInNewtab(url)
+})
+
---
+++ js/link2.js
@@ -0,0 +1,12 @@
+const url = 'https://www.themoviedb.org/movie/76600-avatar-the-way-of-water'
+
+const btn = document.querySelector('#btn2')
+
+function openInNewtab(url) {
+    const win = window.open(url, '_blank')
+    win.focus()
+}
+
+btn.addEventListener('click', () => {
+    openInNewtab(url)
+})
